d9a9a43f297d31e466610fb364f6eb2583db266a
Diogo Lima <diogo@dr6.com.br>
2023-06-01 23:17:56 -0300

    add project image

--- apps/projects/models.py
+++ apps/projects/models.py
@@ -4,8 +4,7 @@ from django.db import models
 class Project(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    image = models.ImageField(upload_to="projects", blank=True, null=True)
 
     def __str__(self):
         return self.name
