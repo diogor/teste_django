638bc30f21d601b53da3f1e5a713d758ed09fc2b
Diogo Lima <diogo@dr6.com.br>
2023-06-07 18:13:44 -0300

    fixx user tests

--- tests/test_user.py
+++ tests/test_user.py
@@ -1,57 +1,49 @@
+from django.shortcuts import get_object_or_404
 from django.contrib.auth.models import Group
 from django.test import TestCase
 from apps.users.models import User
 
 
 class UserCreationTests(TestCase):
-    def test_create_user_has_default_group(self):
-        user = User.objects.create_user(
-            username="testuser",
-            email="test@email.com",
-            password="testpassword",
-            address="testaddress",
-            city="testcity",
-            state="teststate",
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username="test", email="test@test.com", password="test"
         )
-        self.assertEqual(user.groups.count(), 1)
-        self.assertEqual(user.groups.first().name, "Reader")
+
+    def test_create_user_has_default_group(self):
+        self.assertEqual(self.user.groups.count(), 1)
+        self.assertEqual(self.user.groups.first().name, "Reader")
 
     def test_reader_user_cannot_create_project(self):
-        user = User.objects.create_user(
-            username="testuser", email="e@e.com", password="testpassword"
-        )
-        self.assertFalse(user.has_perm("projects.add_project"))
-        self.assertFalse(user.has_perm("projects.change_project"))
-        self.assertFalse(user.has_perm("projects.delete_project"))
+        self.assertFalse(self.user.has_perm("projects.add_project"))
+        self.assertFalse(self.user.has_perm("projects.change_project"))
+        self.assertFalse(self.user.has_perm("projects.delete_project"))
 
     def test_change_user_group_give_perms(self):
-        user = User.objects.create_user(
-            username="testuser", email="e@e.com", password="testpassword"
-        )
-
         group = Group.objects.get(name="Editor")
-        user.groups.add(group)
+        self.user.groups.add(group)
 
-        self.assertTrue(user.has_perm("projects.add_project"))
-        self.assertTrue(user.has_perm("projects.change_project"))
-        self.assertTrue(user.has_perm("projects.delete_project"))
+        self.assertTrue(self.user.has_perm("projects.add_project"))
+        self.assertTrue(self.user.has_perm("projects.change_project"))
+        self.assertTrue(self.user.has_perm("projects.delete_project"))
 
     def test_change_user_group_remove_perms(self):
-        user = User.objects.create_user(
-            username="testuser", email="d@d.com", password="testpassword"
-        )
+        group = Group.objects.get(name="Editor")
 
-        editor_group = Group.objects.get(name="Editor")
+        self.assertFalse(self.user.has_perm("projects.add_project"))
+        self.assertFalse(self.user.has_perm("projects.change_project"))
 
-        self.assertFalse(user.has_perm("projects.add_project"))
-        self.assertFalse(user.has_perm("projects.change_project"))
+        self.user.groups.add(group)
 
-        user.groups.add(editor_group)
+        # necessário para atualizar as permissões evitando o uso de cache
+        user = get_object_or_404(User, pk=self.user.pk)
 
         self.assertTrue(user.has_perm("projects.add_project"))
         self.assertTrue(user.has_perm("projects.change_project"))
 
-        user.groups.remove(editor_group)
+        self.user.groups.remove(group)
 
-        self.assertFalse(user.has_perm("projects.add_project"))
-        self.assertFalse(user.has_perm("projects.change_project"))
+        user = get_object_or_404(User, pk=self.user.pk)
+
+        self.assertFalse(self.user.has_perm("projects.add_project"))
+        self.assertFalse(self.user.has_perm("projects.change_project"))
